daed3369c7a74eb1f1caee754fbf6de2fcd0ae9c
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-09-02 13:53:30 -1200

    kkskks
    kkk

--- src/login.js
+++ src/login.js
@@ -13,16 +13,22 @@ class Login extends React.Component{
 					<div class="bg">
 						<div class="container">
 							<br/>
-							<div class="col-sm-1 col-lg-4"></div>
-							<div class="col-sm-10 col-lg-4">
-								<div class="card">
-									<div class="card-header text-capitalize text-center">
+
+							<div class="col-sm-1 col-md-4 col-lg-0">
+								
+							</div>
+							<div class="col-sm-10 col-md-4 col-lg-12">
+								<div class="card mnb">
+									<div class="card-header text-capitalize text-center"><b><i class="fa fa-users fa-2x" aria-hidden="true"></i></b>&nbsp;&nbsp;
 										Mr.Darshit Gajjar(owner of cismox)
 									</div>
 									<div class="card-body ">
 										<input type="text" class="username" placeholder="example@gmail.com" />
 										<br/><br/>
-										<input type="password" class="password" placeholder="password" />
+										<input type="password" class="password"
+									
+										 placeholder="password"
+										  />
 										<br/>
 										<br/>
 										<br/>
@@ -30,7 +36,7 @@ class Login extends React.Component{
 										<button type="button" class="btn btn-block btn-success lead"><i class="fa fa-cogs fa-fw" aria-hidden="true"></i>
 										&nbsp;&nbsp;<b>Login</b></button>
 										<br/>
-										<a class="forgot d-block m-auto">forgot password?</a>
+										<a class="forgot">forgot password?</a>
 										<br/><br/>
 										<button class="btn btn-block btn-danger">
 											<i class="fa fa-google-plus fa-fw" aria-hidden="true"></i>
@@ -42,7 +48,7 @@ class Login extends React.Component{
 									
 								</div>
 							</div>
-							<div class="col-sm-1 col-lg-4"></div>
+							<div class="col-sm-1 col-md-4 col-lg-0"></div>
 						</div>							
 					</div>
 				</div>
