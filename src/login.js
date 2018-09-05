6040e538638e2559de2018af047cafe711873f1c
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-09-05 11:41:25 -1200

    msmsmsm
    kkk

--- src/login.js
+++ src/login.js
@@ -7,26 +7,47 @@ import icon from './cismox_image/jlk.png';
 import '../node_modules/font-awesome/css/font-awesome.min.css'
 import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
 class Login extends React.Component{
+constructor()
+{
+	super();
+	this.state={
+		email:"",
+		password:"",
+		button:"",
+	}
+	this.email=this.email.bind(this);
+}
+email=(event)=>{
+	this.setState({
+		email:event.target.value,
+		button:"gajjar",
+	})
+}
+password=(eve)=>{
+	this.setState({
+		password:eve.target.value,
+	})
+}
 	render()
-	{
+	{ 
 		return(
 				<div>
 					<div class="bg">
 						<div class="container">
 							<br/>
-
-							<div class="col-sm-1 col-md-4 col-lg-0">
+							<div class="col-sm-0 col-md-4 col-lg-0">
 								
 							</div>
-							<div class="col-sm-10 col-md-4 col-lg-12">
+							<div class="col-sm-12 col-md-4 col-lg-12">
 								<div class="card mnb">
 									<div class="card-header text-capitalize text-center"><b><i class="fa fa-users fa-2x" aria-hidden="true"></i></b>&nbsp;&nbsp;
 										Mr.Darshit Gajjar(owner of cismox)
 									</div>
 									<div class="card-body ">
-										<input type="text" class="username" placeholder="example@gmail.com" />
+									<form>
+										<input type="text" value={this.state.email} onChange={this.email} class="username" placeholder="example@gmail.com" />
 										<br/><br/>
-										<input type="password" class="password"
+										<input type="password" value={this.state.password} onChange={this.password} class="password" id="password"
 									
 										 placeholder="password"
 										  />
@@ -34,13 +55,17 @@ class Login extends React.Component{
 										<br/>
 										<br/>
 										
-										<button type="button" class="btn btn-block btn-success lead"><i class="fa fa-cogs fa-fw" aria-hidden="true"></i>
+										<button type="button" value={this.state.button} disabled={(this.state.email.length&&this.state.password.length)?false:true} onClick={this.active}class="btn btn-block btn-success lead"><i class="fa fa-cogs fa-fw" aria-hidden="true"></i>
 										&nbsp;&nbsp;<b>Login</b></button>
 										<br/>
 										<a class="forgot">forgot password?</a>
-										<br/><br/>
+										<br/>
+
+
+										<br/>
 										
 										<img src={icon} class="img-fluid logo"/>
+									</form>
 									</div>
 								<Link to="/" class="btn btn-info click"><i class="fa fa-chevron-left"></i>&nbsp;&nbsp;Back</Link>
 									
@@ -48,7 +73,7 @@ class Login extends React.Component{
 
 
 							</div>
-							<div class="col-sm-1 col-md-4 col-lg-0"></div>
+							<div class="col-sm-0 col-md-4 col-lg-0"></div>
 						</div>							
 					</div>
 				</div>
